2dbc952ab21d58334d2c3525c2969a2eca9fc077
Natalia Ferreira <nataliaferreira.dev@yahoo.com>
2025-01-08 22:14:06 -0300

    Refatoração: Adicionada a anotação @ResponseStatus para a DeckCreationException com o status BAD_REQUEST.

--- src/main/java/br/com/card_game_api/exception/DeckCreationException.java
+++ src/main/java/br/com/card_game_api/exception/DeckCreationException.java
@@ -1,8 +1,12 @@
 package br.com.card_game_api.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Exceção customizada para erros relacionados à criação de um deck.
  */
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class DeckCreationException extends RuntimeException {
 
     public DeckCreationException(String message) {
